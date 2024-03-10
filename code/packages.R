2790c1e40080e99aaa66722e906153a6ecc59501
nwu_majun <mj200921059@hotmail.com>
2024-03-10 17:08:47 +0800

    Add files via upload

---
+++ code/packages.R
@@ -0,0 +1,80 @@
+setwd("D:/OneDrive - 陕西科技大学/dataanalysis/longevity/data") # laptop
+setwd("D:/OneDrive - 陕西科技大学/dataanalysis/longevity/data_final") 
+# setwd("C:/Users/NWU_M/Documents/OneDrive - 陕西科技大学/dataanalysis/longevity/data")
+setwd("C:/Users/NWU_M/Documents/OneDrive - 陕西科技大学/dataanalysis/longevity/data2") # for desktop
+setwd("C:/Users/NWU_M/Documents/OneDrive - 陕西科技大学/dataanalysis/longevity/data_final")
+#packages
+#JUN MA 3-23-2020
+library("data.table")
+library(dplyr)
+library(stringr)
+library(taxonomizr)
+library(ggplot2)
+library(reshape2) # melt
+library(vegan)
+library(ConQuR)
+library(coda.base)
+library(doParallel)
+library(ggpubr) # ggboxplot
+library(glue)
+library(ggsankey)
+library(mia)
+library(SummarizedExperiment)
+library(SpiecEasi)
+library(ggVennDiagram)
+library(scales)
+library(tidyverse)
+library(RColorBrewer) #brewer.pal
+library(ALDEx2)
+library(Maaslin2)
+library("pheatmap")
+library(ComplexHeatmap)
+
+# library(GUniFrac)
+# library(MicrobiomeStat) # linda
+
+# library(phyloseq)
+#=========================end
+
+# library(apyramid)
+library("tibble")
+library(agricolae)
+# library(devtools)
+# install_github("Displayr/flipPlots")
+# devtools::install_github("davidsjoberg/ggsankey")
+# 
+library(ggsankey)
+# library(ggstatsplot)
+
+# library(ecodist) # bcdist
+library(glue)
+
+#----------------------
+library(DESeq2)
+library("ggplot2")
+library(gridExtra)
+library(scales)
+library(reshape2)
+library(knitr)
+ # 
+library(stringr)
+
+library(plyr)
+library(igraph)
+library(GSA)
+library(piano)
+library(ballgown) # htx
+library(GO.db) # TERM(GOTERM)
+library(apcluster)
+library(KEGGprofile)
+
+library(viridis)
+library(biomaRt)
+library(ggrepel)
+library(enrichR)
+library(igraphdata)
+library(visNetwork)
+
+
+library(VennDiagram)
+library(voronoiTreemap)
