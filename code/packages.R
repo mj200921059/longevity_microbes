cf809a6d6b511facf4a5088a30e24d1c0ceaa7e8
nwu_majun <mj200921059@hotmail.com>
2024-03-10 17:09:30 +0800

    Update packages.R

--- code/packages.R
+++ code/packages.R
@@ -1,8 +1,3 @@
-setwd("D:/OneDrive - 陕西科技大学/dataanalysis/longevity/data") # laptop
-setwd("D:/OneDrive - 陕西科技大学/dataanalysis/longevity/data_final") 
-# setwd("C:/Users/NWU_M/Documents/OneDrive - 陕西科技大学/dataanalysis/longevity/data")
-setwd("C:/Users/NWU_M/Documents/OneDrive - 陕西科技大学/dataanalysis/longevity/data2") # for desktop
-setwd("C:/Users/NWU_M/Documents/OneDrive - 陕西科技大学/dataanalysis/longevity/data_final")
 #packages
 #JUN MA 3-23-2020
 library("data.table")
@@ -36,45 +31,3 @@ library(ComplexHeatmap)
 # library(phyloseq)
 #=========================end
 
-# library(apyramid)
-library("tibble")
-library(agricolae)
-# library(devtools)
-# install_github("Displayr/flipPlots")
-# devtools::install_github("davidsjoberg/ggsankey")
-# 
-library(ggsankey)
-# library(ggstatsplot)
-
-# library(ecodist) # bcdist
-library(glue)
-
-#----------------------
-library(DESeq2)
-library("ggplot2")
-library(gridExtra)
-library(scales)
-library(reshape2)
-library(knitr)
- # 
-library(stringr)
-
-library(plyr)
-library(igraph)
-library(GSA)
-library(piano)
-library(ballgown) # htx
-library(GO.db) # TERM(GOTERM)
-library(apcluster)
-library(KEGGprofile)
-
-library(viridis)
-library(biomaRt)
-library(ggrepel)
-library(enrichR)
-library(igraphdata)
-library(visNetwork)
-
-
-library(VennDiagram)
-library(voronoiTreemap)
